bd57d1d683e4dc6fdcc6c2562d8d9382046e7998
techguysid <s.udeniya23@gmail.com>
2015-09-04 02:54:55 +0530

    testing left

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # unXSS
-Library to provide utility methods to escape and unescape HTML entities.
+=========
+=========
+
+Library to provide utility methods to escape and unescape HTML entitiesand save you from XSS attacks.
+
+## Installation
+
+  npm install scapegoat --save
+
+## Usage
+
+  var scapegoat = require('scapegoat')
+      escape = scapegoat.escape,
+      unescape = scapegoat.unescape;
+
+  var html = '<h1>Hello World</h1>',
+      escaped = escape(html),
+      unescaped = unescape(escaped);
+
+  console.log('html', html, 'escaped', escaped, 'unescaped', unescaped);
+
+## Tests
+
+  npm test
+
---
+++ index.js
@@ -0,0 +1,45 @@
+/**
+ * unxss
+ * https://github.com/techguysid/scapegoat
+ *
+ * Copyright (c) 2015 Siddharth Udeniya
+ * Licensed under the MIT license.
+ */
+var chars = {
+  '&amp;': '&',
+  '&quot;': '"',
+  '&#39;': '\'',
+  '&lt;': '<',
+  '&gt;': '>'
+};
+/**
+ * Escape special characters in the given string of html.
+ *
+ * @param  {String} html
+ * @return {String}
+ */
+module.exports = {
+  escape: function(html) {
+    return String(html)
+      .replace(/&/g, '&amp;')
+      .replace(/"/g, '&quot;')
+      .replace(/'/g, '&#39;')
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;');
+  },
+
+  /**
+   * Unescape special characters in the given string of html.
+   *
+   * @param  {String} html
+   * @return {String}
+   */
+  unescape: function(html) {
+    return String(html)
+      .replace(/&amp;/g, '&')
+      .replace(/&quot;/g, '"')
+      .replace(/&#39;/g, ''')
+      .replace(/&lt;/g, '<')
+      .replace(/&gt;/g, '>');
+  }
+};
